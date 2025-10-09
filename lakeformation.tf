8f66fa5565b6542b051c34f423ae6d79cba6e9f1
Khalid Saifullah <khalid@outlook.com.au>
2025-10-10 09:22:40 +1100

    feat: tag based permission

--- lakeformation.tf
+++ lakeformation.tf
@@ -10,51 +10,78 @@ resource "aws_lakeformation_resource" "this" {
   role_arn = aws_iam_role.lakeformation_service_role.arn
 }
 
-# Permissions for data access role to see and query databases
-resource "aws_lakeformation_permissions" "data_access_database_raw" {
-  permissions = ["DESCRIBE"]
-  principal   = aws_iam_role.lakeformation_data_access.arn
+resource "aws_lakeformation_lf_tag" "sensitivity" {
+  key    = "sensitivity"
+  values = ["pii", "financial", "internal"]
+}
 
+resource "aws_lakeformation_lf_tag" "owner" {
+  key    = "owner"
+  values = ["team_a", "team_b", "team_c"]
+}
+
+resource "aws_lakeformation_resource_lf_tags" "raw_data" {
   database {
     name = aws_glue_catalog_database.raw_data.name
   }
-}
 
-resource "aws_lakeformation_permissions" "data_access_database_processed" {
-  permissions = ["DESCRIBE"]
-  principal   = aws_iam_role.lakeformation_data_access.arn
+  lf_tag {
+    key   = "sensitivity"
+    value = "internal"
+  }
 
-  database {
-    name = aws_glue_catalog_database.processed_data.name
+  lf_tag {
+    key   = "owner"
+    value = "team_b"
   }
 }
 
-resource "aws_lakeformation_permissions" "data_access_database_aggregated" {
-  permissions = ["DESCRIBE"]
-  principal   = aws_iam_role.lakeformation_data_access.arn
-
-  database {
-    name = aws_glue_catalog_database.aggregated_data.name
-  }
-}
-
-# Table-level permissions for data access role
-resource "aws_lakeformation_permissions" "data_access_table" {
-  permissions = ["SELECT", "DESCRIBE"]
-  principal   = aws_iam_role.lakeformation_data_access.arn
-
+resource "aws_lakeformation_resource_lf_tags" "raw_asic_business_names" {
   table {
-    database_name = aws_glue_catalog_table.raw_asic_business_names.database_name
+    database_name = aws_glue_catalog_database.raw_data.name
     name          = aws_glue_catalog_table.raw_asic_business_names.name
   }
+
+  lf_tag {
+    key   = "sensitivity"
+    value = "internal"
+  }
+
+  lf_tag {
+    key   = "owner"
+    value = "team_b"
+  }
 }
 
-# Data location access for data access role
-resource "aws_lakeformation_permissions" "data_access_location" {
-  permissions = ["DATA_LOCATION_ACCESS"]
+resource "aws_lakeformation_permissions" "raw_data" {
   principal   = aws_iam_role.lakeformation_data_access.arn
+  permissions = ["DESCRIBE"]
 
-  data_location {
-    arn = module.lakeformation_s3.s3_bucket_arn
+  lf_tag_policy {
+    resource_type = "DATABASE"
+
+    expression {
+      key    = "sensitivity"
+      values = ["internal"]
+    }
+
+    expression {
+      key    = "owner"
+      values = ["team_c"]
+    }
+  }
+}
+
+resource "aws_lakeformation_permissions" "raw_asic_business_names" {
+  principal   = aws_iam_role.lakeformation_data_access.arn
+  permissions = ["DESCRIBE", "SELECT"]
+
+  lf_tag_policy {
+    resource_type = "TABLE"
+
+    expression {
+      key    = "sensitivity"
+      values = ["internal"]
+    }
   }
 }
